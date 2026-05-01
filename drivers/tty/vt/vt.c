c323651db920b076214bcaa274bd47d4f9b401f6
haohao.brian <brian.chunhao@gmail.com>
2026-05-01 22:43:17 +0800

    Implement con_font_copy for console font management
    Add con_font_copy function to handle console font operations.

--- drivers/tty/vt/vt.c
+++ drivers/tty/vt/vt.c
@@ -4703,7 +4703,26 @@ static int con_font_default(struct vc_data *vc, struct console_font_op *op)
 	}
 	return rc;
 }
+static int con_font_copy(struct vc_data *vc, struct console_font_op *op)
+{
+        int con = op->height;
+        int rc;
 
+
+        console_lock();
+        if (vc->vc_mode != KD_TEXT)
+                rc = -EINVAL;
+        else if (!vc->vc_sw->con_font_copy)
+                rc = -ENOSYS;
+        else if (con < 0 || !vc_cons_allocated(con))
+                rc = -ENOTTY;
+        else if (con == vc->vc_num)     /* nothing to do */
+                rc = 0;
+        else
+                rc = vc->vc_sw->con_font_copy(vc, con);
+        console_unlock();
+        return rc;
+}
 int con_font_op(struct vc_data *vc, struct console_font_op *op)
 {
 	switch (op->op) {
