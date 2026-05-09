5e9a0f593e99c5a0ae6d375f326196fb787fc630
haohao-brian <brian.chunhao@gmail.com>
2026-05-09 15:58:17 +0000

    Update build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 cp ../../kernel_configs/linux-next-default-config .config
-make ARCH=arm64 CC=clang -j$(nproc) KCFLAGS="-march=armv8.5-a+pauth+memtag -flegacy-pass-manager -Xclang -load -Xclang $(realpath scripts/pac-mte/libPMCPass.so)"
+make ARCH=arm64 CC=clang -j$(nproc) 
